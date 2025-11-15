bf4b17f9b16c950f3e8892111971c6b6342f951c
awaisshamsi358 <awaiskhanshamsi358@gmail.com>
2025-11-15 21:34:47 +0430

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar  from './Components/Navbar';
+import InfoAboutme from './Components/InfoAboutme';
+import Contactme from './Components/Contactme';
+import Footer from './Components/Footer';
+function App() {
+  return (
+<>
+      <Navbar />
+      <InfoAboutme />
+      <Contactme />
+      <Footer />
+      </>
+  );
+}
+
+export default App;
